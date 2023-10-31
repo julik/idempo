b0aec6d2e1a60acb1ca76b8ee499a8da6ba45cad
Franz Liedke <249125+franzliedke@users.noreply.github.com>
2023-10-31 21:36:40 +0100

    CI: Try to fix connection to MySQL

--- spec/idempo/active_record_backend_mysql_spec.rb
+++ spec/idempo/active_record_backend_mysql_spec.rb
@@ -6,12 +6,20 @@ require 'mysql2'
 require_relative 'shared_backend_specs'
 
 RSpec.describe Idempo::ActiveRecordBackend do
+  let(:connection) do
+    if ENV['CI']
+      {host: ENV['MYSQL_HOST'], port: ENV['MYSQL_PORT'], adapter: 'mysql2'}
+    else
+      {adapter: 'mysql2'}
+    end
+  end
+
   before :all do
     seed_db_name = Random.new(RSpec.configuration.seed).hex(4)
-    ActiveRecord::Base.establish_connection(adapter: 'mysql2', username: 'root')
+    ActiveRecord::Base.establish_connection(**connection, username: 'root')
     ActiveRecord::Base.connection.create_database('idempo_tests_%s' % seed_db_name, charset: :utf8mb4)
     ActiveRecord::Base.connection.close
-    ActiveRecord::Base.establish_connection(adapter: 'mysql2', encoding: 'utf8mb4', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', username: 'root', database: 'idempo_tests_%s' % seed_db_name)
+    ActiveRecord::Base.establish_connection(**connection, encoding: 'utf8mb4', charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci', username: 'root', database: 'idempo_tests_%s' % seed_db_name)
 
     ActiveRecord::Schema.define(version: 1) do |via_definer|
       Idempo::ActiveRecordBackend.create_table(via_definer)
