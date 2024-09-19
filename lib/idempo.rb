c8bbc89b06185da3433e7c590c1243a389a98a68
Stanislav Katkov <skatkov@cheddar.me>
2024-09-19 16:26:54 +0200

    Any arrable object

--- lib/idempo.rb
+++ lib/idempo.rb
@@ -7,7 +7,7 @@ require "measurometer"
 require "msgpack"
 require "zlib"
 require "set"
-require "rack"
+require 'rack'
 
 require "idempo/version"
 
@@ -56,7 +56,7 @@ class Idempo
       end
 
       status, headers, body = @app.call(env)
-      if Gem::Version.new(Rack.release) >= Gem::Version.new("3.0")
+      if Gem::Version.new(Rack.release) >= Gem::Version.new('3.0')
         # `body` could be of type `ActionDispatch::Response::RackBody` and idempo will not even attempt to store it,
         # we're converting ActionDispatch::Response::RackBody to a storable array format.
         body = body.try(:to_ary) if body.respond_to?(:to_ary)
@@ -123,7 +123,7 @@ class Idempo
   def body_size_within_limit?(response_headers, body)
     return response_headers["Content-Length"].to_i <= SAVED_RESPONSE_BODY_SIZE_LIMIT if response_headers["Content-Length"]
 
-    return false unless body.is_a?(Array) # Arbitrary iterable of unknown size
+    return false unless body.is_a?(Array) || body.respond_to?(:to_ary) # Arbitrary iterable of unknown size
 
     sum_of_string_bytesizes(body) <= SAVED_RESPONSE_BODY_SIZE_LIMIT
   end
