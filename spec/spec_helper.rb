efc464048f77e99bcf03ffbe574f6139dd20f51f
Julik Tarkhanov <me@julik.nl>
2024-11-08 12:59:46 +0000

    Explain the use of the body class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,18 @@
 
 require "idempo"
 
+# Uses as a Rack response body that just generates a certain
+# amount of random bytes. Since we use Rack::Lint in our tests,
+# it will actually check whether we are delivering the body of
+# the size advertised in the content-length HTTP header. Having
+# a body like this is a reasonably fast way to generate those
+# arbitrary responses. It can also accept a Random object so that
+# the generated body data is reproducible.
 class PreSizedBody
+  attr_reader :bytes
   def initialize(size_in_bytes, rng = Random.new)
     @rng = rng
-    @bytes = size_in_bytes
+    @bytes = size_in_bytes.to_i
   end
 
   def each
